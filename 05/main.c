abfdc8a14ccdf4fcc7e4858ac147031da2b73123
Alina Borisova <alina_10112004@mail.ru>
2025-08-02 14:50:24 +0700

    05

---
+++ 05/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
+
+FILE *file;
+int countSIGINT = 0;
+
+void processing(int signal) {
+    if(signal == SIGINT || signal == SIGQUIT) {
+        char *message = (signal == SIGINT) ? "Received SIGINT\n"  : "Received SIGOUT\n";
+        fprintf(file, "%s", message);
+        fflush(file);
+        if(signal == SIGINT) {
+            countSIGINT++;
+            if(countSIGINT == 3) {
+                printf("PROCESS KILLED");
+                fclose(file);
+                exit(0);
+            }
+        }
+    }
+}
+
+int main() {
+    file = fopen("counter.txt", "w");
+    if (file == NULL) {
+        perror("Ошибка открытия файла");
+        return 1;
+    }
+    signal(SIGINT, processing);
+    signal(SIGQUIT, processing);
+    int counter = 1;
+    while (1) {
+        fprintf(file, "%d\n", counter);
+        fflush(file); 
+        counter++;
+        sleep(1);
+    }
+
+    fclose(file);
+    return 0;
+}
